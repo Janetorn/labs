9cc4dd0e0cc38394106edc5f0ba730aa03914f24
Janetorn <me2000zver191614@gmail.com>
2024-12-10 02:39:43 +0300

    Add files via upload

---
+++ lab3_1/lab3_1.cpp
@@ -0,0 +1,60 @@
+﻿#include <iostream>
+#include <vector>
+using namespace std;
+
+class Assembly {
+private:
+    friend class F;
+protected:
+    Assembly() {}
+public:
+    void get() {
+        cout << "Assembly" << endl;
+    }
+};
+
+class Part : public Assembly {
+private:
+    friend class F;
+protected:
+    Part() {}
+public:
+    void get() {
+        cout << "Part" << endl;
+    }
+};
+
+class F {
+public:
+    static Assembly* createAssembly() {
+        return nullptr;
+    }
+
+    static Part* createPart() {
+        return new Part();
+    }
+};
+
+void s() {
+    vector<Assembly*> storage;
+    storage.push_back(F::createAssembly());
+    storage.push_back(F::createPart());
+    for (size_t i = 0; i < storage.size(); i++) {
+        if (storage[i] != nullptr) {
+            storage[i]->get();
+        }
+    }
+
+    Part* p = F::createPart();
+    p->get();
+
+    for (auto& item : storage) {
+        delete item;
+    }
+    delete p;
+}
+
+int main() {
+    s();
+    return 0;
+}
